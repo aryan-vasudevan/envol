269104572553aaf3821ebb21e0f4c91a931da28d
aryan-vasudevan <vasudevan.aryan@gmail.com>
2025-07-19 02:42:03 -0400

    Initial Commit

---
+++ envol/envolApp.swift
@@ -0,0 +1,17 @@
+//
+//  envolApp.swift
+//  envol
+//
+//  Created by Aryan Vasudevan on 2025-07-19.
+//
+
+import SwiftUI
+
+@main
+struct envolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ envolTests/envolTests.swift
@@ -0,0 +1,17 @@
+//
+//  envolTests.swift
+//  envolTests
+//
+//  Created by Aryan Vasudevan on 2025-07-19.
+//
+
+import Testing
+@testable import envol
+
+struct envolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
